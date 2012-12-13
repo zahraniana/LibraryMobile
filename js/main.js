de5217526b666ef5627b51f0aa8c7ca221919240
Abdullah Al-Zahrani <Zahrani@kfupm.edu.sa>
2012-12-13 17:52:21 +0300

    project files added

---
+++ js/main.js
@@ -0,0 +1,52 @@
+﻿function init() {
+ document.addEventListener("deviceready", deviceReady, true);
+ delete init;
+}
+
+function checkPreAuth() {
+    var form = $("#loginForm");
+    if(window.localStorage["username"] != undefined && window.localStorage["password"] != undefined) {
+        $("#username", form).val(window.localStorage["username"]);
+        $("#password", form).val(window.localStorage["password"]);
+        handleLogin();
+    }
+}
+
+function handleLogin() {
+    var form = $("#loginForm");    
+    //disable the button so we can't resubmit while we wait
+    $("#submitButton",form).attr("disabled","disabled");
+    var u = $("#username", form).val();
+    var p = $("#password", form).val();
+    console.log("click");
+    if (u != '' && p != '') {
+
+        $.ajax({
+            url: 'http://staff.kfupm.edu.sa/library/zahrani/SharePointWebService.asmx/AuthenticateUser',
+            data: { userName: u, password: p },
+            dataType: "xml",
+            type: 'GET',
+            success: function (res) {
+                var myXML = res.responseText;
+                // This is the part xml2Json comes in.
+                var JSONConvertedXML = $.xml2json(myXML);
+                alert(JSONConvertedXML);
+                if (JSONConvertedXML == true) {
+                    window.localStorage["username"] = u;
+                    window.localStorage["password"] = p;
+                    navigator.notification.alert("Your login suceeded");
+                    // $.mobile.changePage("some.html");
+                } else {
+                    navigator.notification.alert("Your login failed", function () { });
+                }
+                $("#submitButton").removeAttr("disabled");
+                $.mobile.hidePageLoadingMsg();
+            }
+        });
+    }
+    return false;
+}
+
+function deviceReady() {
+    $("#loginForm").on("submit", handleLogin);
+}
